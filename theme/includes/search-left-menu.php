283b2818744f50717a3dbca09fb4e5a80501b811
Sanjeev <masanjeev@gmail.com>
2019-06-04 12:02:49 +0545

    Theme updates

---
+++ theme/includes/search-left-menu.php
@@ -0,0 +1,122 @@
+<div class="job-grid-sidebar">
+   <div class="single-job-sidebar margin_bottom_5">
+      <div class="job-sidebar-box">
+      <div class="main-title"><svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="search" role="img" width="20px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="svg-inline--fa fa-search fa-w-16 fa-lg"><path fill="currentColor" d="M505 442.7L405.3 343c-4.5-4.5-10.6-7-17-7H372c27.6-35.3 44-79.7 44-128C416 93.1 322.9 0 208 0S0 93.1 0 208s93.1 208 208 208c48.3 0 92.7-16.4 128-44v16.3c0 6.4 2.5 12.5 7 17l99.7 99.7c9.4 9.4 24.6 9.4 33.9 0l28.3-28.3c9.4-9.4 9.4-24.6.1-34zM208 336c-70.7 0-128-57.2-128-128 0-70.7 57.2-128 128-128 70.7 0 128 57.2 128 128 0 70.7-57.2 128-128 128z" class=""></path></svg> Refine Your Job Search</div>
+      </div>
+   </div>                      
+   <!-- Single Job Sidebar Start -->
+   <div class="single-job-sidebar sidebar-location margin_bottom_5">
+      <div class="job-sidebar-box">
+      <div class="search-title">Keywords</div>
+         <p>
+            <input type="text" name="keywords" placeholder="Keywords">
+         </p>
+      </div>
+   </div>
+   <!-- Single Job Sidebar End -->
+   <!-- Single Job Sidebar Start -->
+   <div class="single-job-sidebar sidebar-category margin_bottom_5">
+      <div class="job-sidebar-box">
+         <div class="search-title">Filter by Job Category</div>
+         <select class="sidebar-category-select-2" name="categories[]" multiple="multiple" placeholder="Filter by job category">
+            <option value="1">Information Technology</option>
+            <option value="2">accounting/finance</option>
+            <option value="3">automotive jobs</option>
+            <option value="4">construction</option>
+            <option value="5">design, art & multimedia</option>
+            <option value="6">education training</option>
+            <option value="7">restaurent/food</option>
+            <option value="7">programming/tech</option>
+            <option value="7">sales/marketing</option>
+            <option value="7">data science/analysis</option>
+         </select>
+      </div>
+   </div>
+   <!-- Single Job Sidebar End -->
+   <!-- Single Job Sidebar Start -->
+   <div class="single-job-sidebar sidebar-category margin_bottom_5">
+      <div class="job-sidebar-box">
+         <div class="search-title">Filter by Job Industry</div>
+         <select class="sidebar-category-select-2 sidebar-industry" name="categories[]" multiple="multiple">
+            <option value="1">Information Technology</option>
+            <option value="2">accounting/finance</option>
+            <option value="3">automotive jobs</option>
+            <option value="4">construction</option>
+            <option value="5">design, art & multimedia</option>
+            <option value="6">education training</option>
+            <option value="7">restaurent/food</option>
+            <option value="7">programming/tech</option>
+            <option value="7">sales/marketing</option>
+            <option value="7">data science/analysis</option>
+         </select>
+      </div>
+   </div>
+   <!-- Single Job Sidebar End -->
+   <!-- Single Job Sidebar Start -->
+   <div class="single-job-sidebar sidebar-category margin_bottom_5">
+      <div class="job-sidebar-box">
+         <div class="search-title">Filter by Job Location</div>
+         <select class="sidebar-category-select-2 sidebar-locations" name="categories[]" multiple="multiple">
+            <option value="1">Kathmandu</option>
+            <option value="2">Bhaktapur</option>
+            <option value="3">Lalitpur</option>
+         </select>
+      </div>
+   </div>
+   <!-- Single Job Sidebar End -->
+    
+   <!-- Single Job Sidebar Start -->
+   <div class="single-job-sidebar sidebar-type margin_bottom_5">
+      <div class="job-sidebar-box">
+         <div class="search-title">Filter by Job type</div>
+         <ul>
+            <li class="checkbox">
+               <input class="checkbox-spin" type="checkbox" id="Freelance" />
+               <label for="Freelance"><span></span>Freelance</label>
+            </li>
+            <li class="checkbox">
+               <input class="checkbox-spin" type="checkbox" id="Full_Time" />
+               <label for="Full_Time"><span></span>Full Time</label>
+            </li>
+            <li class="checkbox">
+               <input class="checkbox-spin" type="checkbox" id="Internship" />
+               <label for="Internship"><span></span>Internship</label>
+            </li>
+            <li class="checkbox">
+               <input class="checkbox-spin" type="checkbox" id="Part_Time" />
+               <label for="Part_Time"><span></span>Part Time</label>
+            </li>
+            <li class="checkbox">
+               <input class="checkbox-spin" type="checkbox" id="Temporary" />
+               <label for="Temporary"><span></span>Temporary</label>
+            </li>
+         </ul>
+      </div>
+   </div>
+   <!-- Single Job Sidebar End -->
+   <!-- Single Job Sidebar Start -->
+   <div class="single-job-sidebar sidebar-type margin_bottom_5">
+      <div class="job-sidebar-box">
+         <div class="search-title">Filter by Job Level</div>
+         <ul>
+            <li class="checkbox">
+               <input class="checkbox-spin" type="checkbox" id="entry_level" />
+               <label for="entry_level"><span></span>Entry Level</label>
+            </li>
+            <li class="checkbox">
+               <input class="checkbox-spin" type="checkbox" id="mid_level" />
+               <label for="mid_level"><span></span>Mid Level</label>
+            </li>
+            <li class="checkbox">
+               <input class="checkbox-spin" type="checkbox" id="senior_level" />
+               <label for="senior_level"><span></span>Senior Level</label>
+            </li>
+            <li class="checkbox">
+               <input class="checkbox-spin" type="checkbox" id="top_level" />
+               <label for="top_level"><span></span>Top Level</label>
+            </li>
+         </ul>
+      </div>
+   </div>
+   <!-- Single Job Sidebar End -->                      
+</div>
