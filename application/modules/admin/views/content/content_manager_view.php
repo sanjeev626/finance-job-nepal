65a55c9c8f62fbf6eea2cde5de71b0216a2b6ab8
binaystha <binaya619@gmail.com>
2019-06-04 12:32:02 +0545

    delete modal added

--- application/modules/admin/views/content/content_manager_view.php
+++ application/modules/admin/views/content/content_manager_view.php
@@ -19,8 +19,8 @@
                   <th width="5%">SN.</th>
                   <th width="20%">Title </th>
                   <th width="20%">URL </th>
-                  <th width="20%">Created Date</th>
-                  <th width="20%">Updated Date</th>
+                  <th width="10%">Created Date</th>
+                  <th width="10%">Updated Date</th>
                   <th width="10%" class="table-action">Action</th>
                 </tr>
               </thead>
@@ -48,8 +48,8 @@
                     <td class="table-action">
                       <a class="btn btn-success btn-sm" href="<?php echo base_url(); ?>admin/content/edit/<?php echo $key->id; ?>"><i class="fa fa-edit tooltips" data-original-title="Edit Content"></i> Edit</a>
                       |
-                      <a class="btn btn-danger btn-sm" href="<?php echo base_url(); ?>admin/content/delete/<?php echo $key->id; ?>"><i class="fa fa-trash tooltips" data-original-title="Edit Content"></i> Delete</a>
-                      
+                      <!--<a class="btn btn-danger btn-sm" href="<?php /*echo base_url(); */?>admin/content/delete/<?php /*echo $key->id; */?>"><i class="fa fa-trash tooltips" data-original-title="Edit Content"></i> Delete</a>-->
+                      <button type="button" class="btn btn-danger btn-sm delete_service" link="<?php echo base_url(); ?>admin/content/delete/<?php echo $key->id; ?>" data-toggle="modal" data-target="#myModalDelete"><i class="fa fa-trash tooltips" data-original-title="Delete Service"></i> Delete</button>
                     </td>
                   </tr>
                   <?php
@@ -74,3 +74,38 @@
     <!-- /.box -->
 
   </section>
+
+<!-- Delete Modal -->
+<div id="myModalDelete" class="modal fade" role="dialog">
+  <div class="modal-dialog">
+
+    <!-- Modal content-->
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+        <h4 class="modal-title green">Are you sure to Delete a Content ?</h4>
+      </div>
+      <div class="modal-body center">
+
+        <a class="btn btn-success get_link" href="">Yes</a>
+        &nbsp; | &nbsp;
+        <button type="button" class="btn btn-success" data-dismiss="modal">No</button>
+
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+
+  </div>
+</div>
+
+<script type="text/javascript">
+  $('document').ready(function(){
+    $('.delete_service').on('click',function(){
+      var link  = $(this).attr('link');
+      $('.get_link').attr('href',link);
+
+    });
+  });
+</script>
