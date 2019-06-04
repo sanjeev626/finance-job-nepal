0b532383dcaf966a4f9f195014d74bb54d5e412f
binaystha <binaya619@gmail.com>
2019-06-04 11:09:01 +0545

    fixes

--- application/modules/home/views/includes/footer.php
+++ application/modules/home/views/includes/footer.php
@@ -17,7 +17,7 @@
               <li><a href="<?php echo base_url().'jobseeker/signup#uploadcv'?>"><i class="fa fa-angle-double-right "></i>Direct CV Upload</a></li>
               <li><a href="#"><i class="fa fa-angle-double-right "></i>Online CV</a></li>
               <li><a href="#"><i class="fa fa-angle-double-right "></i>Search Job</a></li>
-              <li><a href="#"><i class="fa fa-angle-double-right "></i>Terms &amp; Conditions</a></li>
+              <li><a href="<?php echo base_url().'content/seeker-terms-conditions'?>"><i class="fa fa-angle-double-right "></i>Terms &amp; Conditions</a></li>
             </ul>
           </div>
         </div>
@@ -33,7 +33,7 @@
                 <?php }
                 ?>
 
-              <li><a href="#"><i class="fa fa-angle-double-right "></i>Terms &amp; Conditions</a></li>
+              <li><a href="<?php echo base_url().'content/employer-terms-conditions'?>"><i class="fa fa-angle-double-right "></i>Terms &amp; Conditions</a></li>
             </ul>
           </div>
         </div>
